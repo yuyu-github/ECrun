97d62220d0c81062a881d30d9bb756a059e5c962
yuyu <hcsd7b34@gmail.com>
2022-01-30 12:50:09 +0900

    モード変更の実装を変更

--- ECrun/Form1.Designer.cs
+++ ECrun/Form1.Designer.cs
@@ -92,6 +92,7 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.DisplayMember = "Name";
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox2.FormattingEnabled = true;
@@ -99,19 +100,19 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(189, 23);
             this.comboBox2.TabIndex = 6;
+            this.comboBox2.ValueMember = "Id";
             // 
             // comboBox1
             // 
+            this.comboBox1.DisplayMember = "Name";
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "環境構築",
-            "実行"});
             this.comboBox1.Location = new System.Drawing.Point(50, 6);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(103, 23);
             this.comboBox1.TabIndex = 7;
+            this.comboBox1.ValueMember = "Id";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // textBox2
--- ECrun/Form1.cs
+++ ECrun/Form1.cs
@@ -12,13 +12,41 @@ namespace Wecres.ECrun
 {
     public partial class Form1 : Form
     {
-        string[] TypesForEnv = {
-            "React",
-        };
-        string[] TypesForRun =
+       Dictionary<string, string[]> Types = new Dictionary<string, string[]>
         {
-            "Reac",
+            {
+                "BuildEnv", new string[]{
+                    "React",
+                }
+            },
+            {
+                "Run", new string[] {
+                    "Reac",
+                }
+            },
         };
+
+        class ComboBoxItems
+        {
+            public class ComboBoxItem
+            {
+                public string Name { get; set; }
+                public string Id { get; set; }
+            }
+
+            public List<ComboBoxItem> List { get; set; } = new List<ComboBoxItem>();
+
+            public ComboBoxItems(string[,] Items)
+            {
+                for (int i = 0; i < Items.Length / 2; i++)
+                {
+                    var Item = new ComboBoxItem();
+                    Item.Name = Items[i, 0];
+                    Item.Id = Items[i, 1];
+                    this.List.Add(Item);
+                }
+            }
+        }
 
         public Form1()
         {
@@ -27,28 +55,23 @@ namespace Wecres.ECrun
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            comboBox1.DataSource = new ComboBoxItems(new string[,] {
+                { "環境構築", "BuildEnv" },
+                { "実行", "Run" },
+            }).List;
             comboBox1.SelectedIndex = 0;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var Items = new string[0];
-            switch (comboBox1.SelectedIndex)
-            {
-                case 0:
-                    Items = TypesForEnv;
-                    break;
-                case 1:
-                    Items = TypesForRun;
-                    break;
-            }
+            var Items = Types[comboBox1.SelectedValue.ToString()];
 
             comboBox2.Items.Clear();
             foreach (var Item in Items)
             {
                 comboBox2.Items.Add(Item);
             }
-            comboBox2.SelectedIndex = 0;
+            if (comboBox2.Items.Count != 0) comboBox2.SelectedIndex = 0;
         }
 
         private void button2_Click(object sender, EventArgs e)
