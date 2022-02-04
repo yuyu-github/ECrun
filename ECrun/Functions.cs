6a130491b4e3b492fb5038958a1167c905650bfe
yuyu <hcsd7b34@gmail.com>
2022-02-04 20:59:22 +0900

    Change: index.html生成時の内容を変更
    metaでviewportを指定するよう変更

--- ECrun/Functions.cs
+++ ECrun/Functions.cs
@@ -52,6 +52,7 @@ npm install typescript");
                                                 File.WriteAllText(@"dist\index.html", @"<!DOCTYPE html>
 <html lang=""ja"">
 <head>
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
     <meta charset=""UTF-8"">
     <title></title>
     <link rel=""stylesheet"" href=""style.css"">
